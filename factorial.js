e3e321f07ab1924392d1e8aa4fb0834eaea8e2f8
cliffamadeus <dev.clfm5t12@gmail.com>
2025-09-08 08:21:05 +0800

    Wrapped the console application within the mainMenu() function

--- factorial.js
+++ factorial.js
@@ -5,41 +5,43 @@ const rl = readline.createInterface({
     output:process.stdout
 })
 
-console.log("Factorial Application");
+function mainMenu(){
+    console.log("Factorial Application");
 
-console.log("1. Say Hello");
-console.log("2. Compute Factorial");
-console.log("3. Exit");
+    console.log("1. Say Hello");
+    console.log("2. Compute Factorial");
+    console.log("3. Exit");
 
-rl.question("Enter your choice (1-3)", choice =>{
-    
-    if(choice==="1"){
-        console.log("Hello There!");
-    } else if (choice==="2"){
-        console.log("Factorial");
+    rl.question("Enter your choice (1-3)", choice =>{
         
-        rl.question("Enter a number for factorial: ", (numStr) =>{
-            let num = parseInt(numStr);
+        if(choice==="1"){
+            console.log("Hello There!");
+        } else if (choice==="2"){
+            console.log("Factorial");
+            
+            rl.question("Enter a number for factorial: ", (numStr) =>{
+                let num = parseInt(numStr);
 
-            if(isNaN(num) || num<0){
-                console.log("Please enter a non-negative integer");
-            } else {
+                if(isNaN(num) || num<0){
+                    console.log("Please enter a non-negative integer");
+                } else {
 
-                let fact = 1;
+                    let fact = 1;
 
-                for (let i = 1; i <= num ; i++){
-                    fact *=i;
+                    for (let i = 1; i <= num ; i++){
+                        fact *=i;
+                    }
+
+                    console.log("The factorial of "+ num + " is " + fact);
                 }
+            });
 
-                console.log("The factorial of "+ num + " is " + fact);
-            }
-        });
+        } else if (choice==="3"){
+            console.log("Exiting program...Goodbye");
+            rl.close();
+        } else{
+            console.log("Invalid choice. Please Try Again");
+        }
 
-    } else if (choice==="3"){
-        console.log("Exiting program...Goodbye");
-        rl.close();
-    } else{
-        console.log("Invalid choice. Please Try Again");
-    }
-
-})
+    })
+}
