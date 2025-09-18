fc11aeb0baf8487a01f2d3c1400ffb39f9f359e3
cliffamadeus <dev.clfm5t12@gmail.com>
2025-09-18 08:06:07 +0800

    Added performAbility() as abstraction sample

--- hero.js
+++ hero.js
@@ -10,9 +10,18 @@ class Hero{
     getHealth(){
         return this.#health;
     }
+
+    getStats(){
+        console.log("\n");
+        console.log("Name: " + this.name);
+        console.log("Health: " + this.#health);
+        console.log("Attack: " +this.attack)
+        
+    }
 }
 
 class Warrior extends Hero{
+
     useAbility(){
         console.log(`${this.name} uses power Strike`);
     }
@@ -28,8 +37,20 @@ class Mage extends Hero{
     }
 }
 
-const warrior = new Warrior("Thorin",100,10);
-warrior.useAbility();
 
-const mage = new Mage("Gandalf",80,8,50);
-mage.useAbility();
+const Thorin = new Warrior("Thorin",100,10);
+//Thorin.useAbility();
+Thorin.getStats();
+
+const Gandalf = new Mage("Gandalf",80,8,50);
+//Gandalf.useAbility();
+Gandalf.getStats();
+
+
+function performAbility(hero){
+    console.log("\n");
+    hero.useAbility();
+}
+
+performAbility(Thorin);
+performAbility(Gandalf);
